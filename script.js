ae3f9e506a88f2a1e033e505517dbb4a57ddee93
Ali Reza Khazaei Nezhad <khazaeinezhadalireza@gmail.com>
2022-07-13 13:36:59 +0430

    sync

---
+++ script.js
@@ -0,0 +1,98 @@
+// Learn 03
+// function ShowDiv(){
+//     $("div").show();
+// }
+
+// function HideDiv(){
+//     $("div").hide();
+// }
+
+
+
+
+
+
+
+
+
+// Learn 04
+// function ShowDiv(){
+//     $(".divItem").show();
+// }
+
+// function HideDiv(){
+//     $(".divItem").hide();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Learn 05
+// function ShowDiv(){
+//     $(".red").show();
+// }
+
+// function HideDiv(){
+//     $(".red").hide();
+// }
+
+
+
+
+
+
+
+
+
+
+// Learn 06
+// function FadeInDiv(){
+//     $(".red").fadeIn();
+// }
+
+// function FadeOutDiv(){
+//     $(".red").fadeOut();
+// }
+
+
+
+
+
+
+
+
+// Learn 07
+// function FadeInDiv(){
+//     $(".red").fadeIn("slow");
+// }
+
+// function FadeOutDiv(){
+//     $(".red").fadeOut("slow");
+// }
+
+
+
+
+
+
+
+
+
+//Learn 08
+// function FadeInDiv(){
+//     $(".red").fadeIn(5000);
+// }
+
+// function FadeOutDiv(){
+//     $(".red").fadeOut(3000);
+// }
